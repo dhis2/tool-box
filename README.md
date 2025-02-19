410c0b531b9beb274a42dc7499c965b70d6a428f
Jason Pickering <jason@dhis2.org>
2025-02-19 12:26:33 +0100

    Update README

--- README.md
+++ README.md
@@ -1,13 +1,29 @@
-# DHIS2 App Template for Dummies
-Basic DHIS2 app for simple tools.
+# DHIS2 Admin Toolbox
+Basic toolbox for system administrators to perform common tasks in DHIS2.
 
 > **WARNING**
 > This tool is intended to be used by system administrators to perform specific tasks, it is not intended for end users. It is available as a DHIS2 app, but has not been through the same rigorous testing as normal core apps. It should be used with care, and always tested in a development environment.
 
-
 ## License
-© Copyright University of Oslo 2024
+© Copyright University of Oslo 2025
 
+## Features
+
+This app provides you a central place to view tools which have been released for DHIS2 System administrators
+by the DHIS2 Global Implementation team. The tools are available as DHIS2 apps, and can be installed in your DHIS2 instance.
+
+You will see a list of tools which are available on GitHub, which versions you have installed on your instance.
+
+Because of limitations with Cross object resource sharing (CORS), it is not possible to install the app directly into your instance. You will need to download the app from GitHub, and install it manually using the App Management App. The DHIS2 Toolbox will provide you with a link to the GitHub repository where you can download the latest versions of all tools.
+
+## Using this app
+### Install a GitHub personal access token
+
+To use this app, you need to create a personal access token on GitHub. This is used to access the GitHub API to fetch the latest releases of the DHIS2 apps. Github has a limit on the number of requests that can be made without authentication, so this is necessary to avoid hitting that limit.
+
+Head over to Github and create a new personal access token. You can do this by going to your account settings, then Developer settings, then Personal access tokens. Click on "Generate new token" and give it a name, and select the `repo` scope. Copy the token and save it somewhere safe.
+
+A copy of this key in your `userDataStore` in DHIS2, so that you don't have to enter it every time you use the app. Note that this token is stored in the `userDataStore` in DHIS2, and is only accessible to the current user. It is not shared with other users. If you want to remove the token, you can make a DELETE request to the same endpoint.
 
 ## Getting started
 
@@ -44,3 +60,4 @@ A different DHIS2 instance can be used to develop against by adding a `d2auth.js
     "password": "District1!"
 }
 ```
+
--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 "use strict";
 import "./css/style.css";
-import { d2Get } from "./js/d2api.js";
+import { d2Get, d2Delete, d2PostJson } from "./js/d2api.js";
 
 const tools_repos = [
     "dhis2/tool-dashboard-pruner",
@@ -16,15 +16,29 @@ const tools_repos = [
 
 var latest_releases = [];
 var installed_apps = [];
-async function fetchGitHubReleases(repo) {
-    var res = await fetch(`https://api.github.com/repos/${repo}/releases`);
+
+async function fetchGitHubReleases(repo, key) {
+    var res = await fetch(`https://api.github.com/repos/${repo}/releases`, {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${key}`,
+            "Accept": "application/vnd.github+json"
+        }
+    });
     var data = await res.json();
     return data;
 }
 
-async function fetchToolName(repo){
+async function fetchToolName(repo, github_key){
 
-    const repo_contents_res = await fetch(`https://api.github.com/repos/${repo}/contents`);
+    const repo_contents_res = await fetch(`https://api.github.com/repos/${repo}/contents`, {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${github_key}`,
+            "Accept": "application/vnd.github+json"
+        }
+    });
+
     var repo_contents = await repo_contents_res.json();
     var package_json = repo_contents.find(file => file.name === "package.json");
     var package_json_file = await fetch(package_json.download_url);
@@ -38,30 +52,105 @@ async function fetchToolName(repo){
 }
 
 
-async function getLatestToolReleases() {
+async function getLatestToolReleases(github_key) {
     const releases = await Promise.all(tools_repos.map(async repo => {
-        const release_info = await fetchGitHubReleases(repo);
-        const tool_name = await fetchToolName(repo);
+        const release_info = await fetchGitHubReleases(repo, github_key);
+        const tool_name = await fetchToolName(repo, github_key);
         const latest_release = release_info[0];
         return {
             repo: repo,
-            published_at: latest_release.published_at,
-            download_url: latest_release.assets[0].browser_download_url,
-            name: tool_name.name,
-            version: tool_name.version
+            published_at: latest_release ? latest_release.published_at : "unknown",
+            download_url: latest_release && latest_release.assets.length > 0 ? latest_release.assets[0].browser_download_url : "unknown",
+            name: tool_name.name || "unknown",
+            version: tool_name.version || "unknown"
         };
     }));
-    return releases;
+    const now = new Date().toISOString();
+    return {
+        fetched_at: now,
+        releases: releases
+    };
 }
 
+async function getUserDataStoreKey() {
+    try {
+        const key = await d2Get("/userDataStore/dhis2-toolbox/github-key");
+        return key;
+    } catch (error)
+    {
+        console.error("Failed to fetch github key from userDataStore:", error);
+        return null;
+    }
+}
+
+async function  saveLatestReleasesToDataStore(latest_releases) {
+    try {
+        await d2Delete("/dataStore/dhis2-toolbox/latest_releases");
+    } catch (error) {
+        console.warn("Failed to delete latest releases from dataStore:", error);
+    }
+    await d2PostJson("/dataStore/dhis2-toolbox/latest_releases", latest_releases);
+}
+
+function renderModalWindowTokenInput() {
+    var modalContent = document.getElementById("modalContent");
+    modalContent.innerHTML = `<div id="modalContent">
+        <h2>Enter your GitHub key to get started</h2>
+        <p>Be sure to consult the README for this app. Once you have created
+        a GitHub token, paste it here to get started. Your key will be stored
+        in the user data store, and only visible to your user.</p>
+        <input type="text" id="githubKey" placeholder="Enter your GitHub key">
+        <button id="saveKey">Save</button>
+    </div>`;
+    document.getElementById("saveKey").addEventListener("click", async () => {
+        const github_key = document.getElementById("githubKey").value;
+        await d2PostJson("/userDataStore/dhis2-toolbox/github-key", github_key);
+        window.location.reload();
+    });
+}
+
+async function testGitHubWorksAuthenticated(github_key) {
+    try {
+        const res = await fetch("https://api.github.com/", {
+            headers: {
+                "Authorization": `Bearer ${github_key}`,
+                "Accept": "application/vnd.github+json"
+            }
+        });
+        if (res.status === 200) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    catch (error) {
+        console.error("Failed to test GitHub authentication:", error);
+        return false;
+    }
+}
+
+
 function renderToolReleases(latest_releases, installed_apps) {
-  
     var html = "";
+    //Top level button to refresh the releases
+    html += "<button id=\"updateReleases\">Update Releases</button>";
+    html += "<p> Last updated on:" + new Date(latest_releases.fetched_at).toLocaleDateString(
+        "en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric"
+        }
+    ) + "</p>";
+    html += "<h2>Tool Releases</h2>";
+
     //Sort the tools by name
-    latest_releases.sort((a, b) => a.name.localeCompare(b.name));
-    latest_releases.forEach(tool => {
-        //Check if the tool is installed
-        var installed = installed_apps.find(app => app.key.lowerCase === tool.name.lowerCase);
+    latest_releases.releases.sort((a, b) => (a.name || "").localeCompare(b.name || ""));
+    latest_releases.releases.forEach(tool => {
+
+        const installed = installed_apps.find(app => app.name.toLowerCase() === tool.name.toLowerCase());
         const publishedDate = new Date(tool.published_at).toLocaleDateString("en-US", {
             year: "numeric",
             month: "long",
@@ -73,7 +162,7 @@ function renderToolReleases(latest_releases, installed_apps) {
             <p><a href="https://github.com/${tool.repo}" target="_blank">Github repo<a> </h3>
             <p>Installed version: ${installed ? installed.version : "Not installed"}</p>
             <p>Current Version: ${tool.version}</p>
-            <p>Published: ${publishedDate}</p>
+            <p>Released on: ${publishedDate}</p>
             <a href="${tool.download_url}">Download</a>
         </div>`;
     });
@@ -87,7 +176,42 @@ window.getLatestToolReleases = getLatestToolReleases;
 window.latest_releases = latest_releases;
 
 document.addEventListener("DOMContentLoaded", async function () {
-    installed_apps = await d2Get("/apps");
-    latest_releases = await getLatestToolReleases();
-    renderToolReleases(latest_releases, installed_apps);
+    //Get the users github key
+    const github_key = await getUserDataStoreKey();
+    //Warn the user if the key is not present
+    if (!github_key) {
+        renderModalWindowTokenInput();
+    } else {
+
+        //Check if the key is valid
+        const github_key_valid = await testGitHubWorksAuthenticated(github_key);
+        if (!github_key_valid) {
+            alert("The GitHub key you have entered is invalid. Please enter a valid key.");
+            renderModalWindowTokenInput();
+            return;
+        }
+        installed_apps = await d2Get("/apps");
+        //Check if the latest releases are already stored in the dataStore
+        try {
+            latest_releases = await d2Get("/dataStore/dhis2-toolbox/latest_releases");
+        } catch (error) {
+            console.error("Failed to fetch latest releases from dataStore:", error);
+            latest_releases = null;
+        }
+
+        if (!latest_releases) {
+            latest_releases = await getLatestToolReleases(github_key);
+            await saveLatestReleasesToDataStore(latest_releases);
+        }
+        renderToolReleases(latest_releases, installed_apps);
+        //Render button to update the datastore with latest releases
+        document.getElementById("updateReleases").addEventListener("click", async () => {
+            installed_apps = await d2Get("/apps");
+            latest_releases = await getLatestToolReleases(github_key);
+            await saveLatestReleasesToDataStore(latest_releases);
+            renderToolReleases(latest_releases, installed_apps);
+        });
+
+    }
+
 }   );
